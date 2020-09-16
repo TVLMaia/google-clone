637cdc9aa004433182f8f264fa41a63cedf7b64e
TVLMaia <56745146+TVLMaia@users.noreply.github.com>
2020-09-16 11:15:11 -0300

    Inico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando essa bodega monstruosa parceiro do mal </h1>
+
+    {/* Home (the one with the search one) */}
+
+    <Home /> 
+    {/* SearchPage (the one with results one) */}
+
     </div>
   );
 }
 
-export default App;
+export default App; 
